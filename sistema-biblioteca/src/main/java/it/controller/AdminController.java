9f0057f2bb00174ade508353a0d78618afa3c1d1
Angelo-Peruzzi <angeloperu98@gmail.com>
2026-04-15 11:48:50 +0200

    insert copia libro

--- sistema-biblioteca/src/main/java/it/controller/AdminController.java
+++ sistema-biblioteca/src/main/java/it/controller/AdminController.java
@@ -1,47 +1,50 @@
 package it.controller;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.dto.BookDto;
+import it.dto.UserDto;
 import it.entity.Book;
+import it.entity.User;
 import it.exception.NoIsbnFoundException;
 import it.service.BookService;
+import it.service.UserService;
 
 @Controller
 public class AdminController {
 	
 	private final BookService bookService;
-	public AdminController(BookService bookService) {
+	private final UserService userService;
+	public AdminController(BookService bookService, UserService userService) {
 		this.bookService = bookService;
+		this.userService = userService;
 	}
 	
 	
-	@PostMapping("/admin/gestione/aggiungi")
-	public String addBook(@RequestParam String username, 
-						  @RequestParam String role,
-						  @RequestParam String azione,
-						  @RequestParam String isbn,
-						  Model model) {
-		
-		
+	@GetMapping("/api/addBook")
+	public String addBookone(
+			@RequestParam(value = "email",required = false) String email, 
+			@RequestParam(value = "isbn", required = false) String isbn,
+			Model model) {
+		UserDto user = userService.getUserByEmail(email);
 		int res = 0;
-		model.addAttribute("username", username);
-		model.addAttribute("role", role);
-		System.out.println(username);
-		if(azione.equals("aggiungi")) {
-			try {
+		System.out.println(isbn + " " + email);
+        if (user == null) {
+            return "redirect:/";
+        }
+        	try {
 				BookDto bookDto= new BookDto(isbn);
 				res = bookService.addBook(bookDto.getIsbnCode());
 				model.addAttribute("addBook", "hai inserito" + " " + res + " libro" );
-				return "layout";
+				System.out.println("test user e isbn" + user.getUserEmail() +"&"+ bookDto.getIsbnCode());
 			}catch(NoIsbnFoundException ex) {
 				model.addAttribute("insertFallitaException", ex.ToString());
-				return "layout";
-			}
-		}	
-		return "layout";
+				
+			}		
+		return "redirect:/dashboard?email=" + user.getUserEmail() + "&section=catalog";
 	}
 	
 }
