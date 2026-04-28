003e3109f14b20da43bfa8813cd160cd4ae98f99
Tubal3h <2bal3h@gmail.com>
2026-04-28 11:46:47 +0200

    fix book history

--- sistema-biblioteca/src/main/java/it/repository/RentRecordRepository.java
+++ sistema-biblioteca/src/main/java/it/repository/RentRecordRepository.java
@@ -270,13 +270,13 @@ public class RentRecordRepository implements RentRecordRepositoryInterface {
 	@Override
 	public List<BookRecordsJoinDtoResponse> getBookRecords(int bookId) throws HistoryNotFoundException {
 	    String sql = """
-	    		SELECT b.book_id, r.rental_id, u.user_name, u.user_last_name, r.booking_date, r.rental_date, r.rental_expired, r.rental_ended, bn.title
+	    		SELECT r.book_id, r.rental_id, u.user_name, u.user_last_name, r.booking_date, r.rental_date, r.rental_expired, r.rental_ended, bn.title
 	    		FROM rental_record r
-	    		INNER JOIN users u ON r.users_id = u.users_id
-	    		INNER JOIN books b ON b.book_id = r.book_id
+	    		LEFT JOIN users u ON r.users_id = u.users_id
+	    		LEFT JOIN books b ON b.book_id = r.book_id
                 LEFT JOIN edition e ON b.edition_id = e.edition_id
                 LEFT JOIN books_names bn ON e.book_name_id = bn.book_name_id
-	    		WHERE b.book_id = ?
+	    		WHERE r.book_id = ?
                 ORDER BY r.rental_id DESC
 	    """;
 	    
@@ -290,13 +290,13 @@ public class RentRecordRepository implements RentRecordRepositoryInterface {
 
     public List<BookRecordsJoinDtoResponse> getUserRecords(int userId) throws HistoryNotFoundException {
         String sql = """
-                SELECT b.book_id, r.rental_id, u.user_name, u.user_last_name, r.booking_date, r.rental_date, r.rental_expired, r.rental_ended, bn.title
+                SELECT r.book_id, r.rental_id, u.user_name, u.user_last_name, r.booking_date, r.rental_date, r.rental_expired, r.rental_ended, bn.title
                 FROM rental_record r
-                INNER JOIN users u ON r.users_id = u.users_id
-                INNER JOIN books b ON b.book_id = r.book_id
+                LEFT JOIN users u ON r.users_id = u.users_id
+                LEFT JOIN books b ON b.book_id = r.book_id
                 LEFT JOIN edition e ON b.edition_id = e.edition_id
                 LEFT JOIN books_names bn ON e.book_name_id = bn.book_name_id
-                WHERE u.users_id = ?
+                WHERE r.users_id = ?
                 ORDER BY r.rental_id DESC
         """;
         
